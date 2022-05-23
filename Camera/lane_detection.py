25ae2eea0e2280c9549bcab4207621304e4cc669
crispTesting <86479081+crispTesting@users.noreply.github.com>
2022-05-23 11:17:05 +0200

    Add files via upload

--- Camera/lane_detection.py
+++ Camera/lane_detection.py
@@ -1,3 +1,4 @@
+
 #############
 #
 #   IMAGE PROCESSING FOR CIRSP
@@ -8,9 +9,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 import time
 from utilities import *
+import math
 
 # Absolute path to image
-path = r'C:\Users\bjorn\Documents\CriSp\images\test_images\test_3.jpeg'
+path = r'C:\Users\bjorn\Documents\CriSp\images\t_29_march\2.jpeg'
 # Loading the image from path
 img = cv2.imread(path)
 # Crop dimensions for faster image processing
@@ -25,21 +27,40 @@ img_org_resized = resize(img, crop_dimensions)
 # Copy of original, used for drawing on.
 img_org_resized_2 = resize(img, crop_dimensions)
 
+
 # Calculating horizontals
 height = img_processed.shape[0]
-first_horizontal = int(height * 0.8) 
-second_horizontal = int( height * 0.4)
-third_horizontal = int (height * 0.2)
+first_horizontal = int(height*0.8)    # 0.8 # height-1
+second_horizontal = int( height * 0.4)  # 0.4 # 0.9
+third_horizontal = int (height * 0.2)   # 0.2 # 0.8
+
+#
+#fourth_horizontal = int(height * 0.7) 
+#fifth_horizontal = int( height * 0.6)
+#sixth_horizontal = int (height * 0.55)
+
+#for i in range(img_processed.shape[0]):
+#    for j in range(141, -1, -1):
+#        img_processed[i][j] = 0
+
+#for i in range(138, 320, 1):
+#    for j in range(38):
+#        img_processed[j][i] = 0
 
 # Getting left and right lanes
 h1_l, h1_r = scan_for_lane_mid(first_horizontal, img_processed)
 h2_l, h2_r = scan_for_lane_mid(second_horizontal, img_processed)
 h3_l, h3_r = scan_for_lane_mid(third_horizontal, img_processed)
 
+#h4_l, h4_r = scan_for_lane_mid(fourth_horizontal, img_processed)
+#h5_l, h5_r = scan_for_lane_mid(fifth_horizontal, img_processed)
+#h6_l, h6_r = scan_for_lane_mid(sixth_horizontal, img_processed)
+
 # Calculating middle points for all horizontals
 m1 = int( (h1_r - h1_l) / 2) + h1_l
 m2 = int( (h2_r - h2_l) / 2) + h2_l
 m3 = int( (h3_r - h3_l) / 2) + h3_l
+
 
 print(f"First:\t{h1_l} - {h1_r}")
 print(f"Second:\t{h2_l} - {h2_r}")
@@ -47,8 +68,9 @@ print(f"Third:\t{h3_l} - {h3_r}")
 
 # List of horizonbtals and left and right lane pixels
 horizontals = [[first_horizontal, h1_l, h1_r], [second_horizontal, h2_l, h2_r], [third_horizontal, h3_l, h3_r]]
+#horizontals = [[first_horizontal, h1_l, h1_r], [second_horizontal, h2_l, h2_r], [third_horizontal, h3_l, h3_r], [fourth_horizontal, h4_l, h4_r], [fifth_horizontal, h5_l, h5_r], [sixth_horizontal, h6_l, h6_r]]
 # Same as above -- this should probably be refactored instead of having lot of magic code
-horizontals2 = [[first_horizontal, h1_l, h1_r], [second_horizontal, h2_l, h2_r]]
+#horizontals2 = [[first_horizontal, h1_l, h1_r], [second_horizontal, h2_l, h2_r]]
 # List of middlepoints
 middlepoints = [m1, m2]
 
@@ -57,10 +79,32 @@ middlepoints = [m1, m2]
 # Should make a picture of the track and measure how far do I see. And use that in the calculations of speed and so on.
 #hypotenuse = (h2_r - h1_r) / (second_horizontal - first_horizontal)
 
+I_mid = img_processed.shape[1] / 2
+
+lateral_error = m1 - I_mid 
+print(f"Lateral error:\t{lateral_error}")
+
+I_mid_vector = [I_mid, img_processed.shape[0]]
+print(f"I_mid vector:\t{I_mid_vector}")
+
+slope = (second_horizontal-first_horizontal)/(m2-m1) 
+
+print(f"SLOPE:\t{slope}")
+
+heading_angle = math.atan2(first_horizontal*I_mid-m1*img_processed.shape[0], m1*I_mid+first_horizontal*img_processed.shape[0])
+print(f"HEADING ANLGE:\t{math.degrees(heading_angle)}")
+
+# Calculating the converstional constant between pixels and cm
+h1_c = get_pw_to_cm_conversion_constant(h1_r, h1_l)
+h2_c = get_pw_to_cm_conversion_constant(h2_r, h2_l)
+h3_c = get_pw_to_cm_conversion_constant(h3_r, h3_l)
+print(f"\nPW to CM:\nFirst:\tSecond:\tThird:\n{h1_c}\t{h2_c}\t{h3_c}")
+
+
 # Drawing the horizontals
 img_draw = draw_horizontals(img_org_resized, horizontals)
 # Drawing a heading vector on the image.
-draw_heading(img_draw, horizontals2, middlepoints)
+draw_heading(img_draw, horizontals, middlepoints)
 # Making the binary image have 3_channels - needed for displaying
 img_processed_3_channel = cv2.cvtColor(img_processed, cv2.COLOR_GRAY2BGR)
 # Stacking the images in a stack before displaying
--- Camera/utilities.py
+++ Camera/utilities.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import math
 
 def scan_for_lane_mid(horizontal, img):
     '''
@@ -25,6 +26,7 @@ def scan_for_lane_mid(horizontal, img):
 
     # Finding start of left lane. Scaning from middle to left.
     for i in range(mid, 0, -1):
+    #for i in range(int(width*0.9), 0, -1):
         if img[horizontal][i] == LANE and left_lane == 0:
             left_lane = i
 
@@ -55,7 +57,8 @@ def process_image(img, crop_dimensions, kernel):
     grey = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
     gaussian = cv2.GaussianBlur(grey, (7,7), 1)
     ret1, th1 = cv2.threshold(gaussian, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_TRIANGLE)
-    dilated = dilate(th1, kernel)
+    eroded = erode(th1, kernel)
+    dilated = dilate(eroded, kernel)
     return dilated
 
 def erode(img, kernel, iter=1):
@@ -151,9 +154,12 @@ def draw_horizontals(img, horizontals):
         if (horizontals[i][2]==0):
             horizontals[i][2] = horizontals[i][1]
 
-    cv2.line(img, (horizontals[0][1], horizontals[0][0]), (horizontals[0][2], horizontals[0][0]), color=(255, 0, 0), thickness=4)
-    cv2.line(img, (horizontals[1][1], horizontals[1][0]), (horizontals[1][2], horizontals[1][0]), color=(0, 255, 0), thickness=4)
-    cv2.line(img, (horizontals[2][1], horizontals[2][0]), (horizontals[2][2], horizontals[2][0]), color=(0, 0, 255), thickness=4)
+    cv2.line(img, (horizontals[0][1], horizontals[0][0]), (horizontals[0][2], horizontals[0][0]), color=(255, 0, 0), thickness=2)
+    cv2.line(img, (horizontals[1][1], horizontals[1][0]), (horizontals[1][2], horizontals[1][0]), color=(0, 255, 0), thickness=2)
+    cv2.line(img, (horizontals[2][1], horizontals[2][0]), (horizontals[2][2], horizontals[2][0]), color=(0, 0, 255), thickness=2)
+    #cv2.line(img, (horizontals[3][1], horizontals[3][0]), (horizontals[3][2], horizontals[3][0]), color=(0, 0, 255), thickness=2)
+    #cv2.line(img, (horizontals[4][1], horizontals[4][0]), (horizontals[4][2], horizontals[4][0]), color=(0, 0, 255), thickness=2)
+    #cv2.line(img, (horizontals[5][1], horizontals[5][0]), (horizontals[5][2], horizontals[5][0]), color=(0, 0, 255), thickness=2)
     return img
 
 def draw_heading(img, horizontals, middlepoints):
@@ -176,7 +182,74 @@ def draw_heading(img, horizontals, middlepoints):
     '''
     cv2.line(img,(middlepoints[0], horizontals[0][0]), (middlepoints[1], horizontals[1][0]), color=(255, 60, 140), thickness=2 )
     cv2.line(img,(horizontals[0][1], horizontals[0][0]), (horizontals[1][1], horizontals[1][0]), color=(255, 180, 60), thickness=2 )
+    cv2.line(img,(int(img.shape[1]/2), 0), (int(img.shape[1]/2), img.shape[0]), color=(255, 180, 60), thickness=2 )
     return img
+
+def calculate_lateral_error(horizontals, midpoints):
+    #################################################################
+    #                                                               #
+    #       ALGORITHM                                               #                 
+    #                                                               #
+    #   1. Find two points on the left lane                         #
+    #                                                               #
+    #   2. Find the middle points of two horizontals                #
+    #                                                               #
+    #   3. Calculate the angular difference between them.           #
+    #                                                               #
+    #   4. Return the angle rounded to integer                      #
+    #                                                               #
+    #################################################################
+    pass
+
+def get_lateral_error():
+    '''
+        The lateral error will be the distance from the edge of the lane
+        to the middle point of the lane.
+
+        This ground truth of this distance will have to be calculated beforehand,
+        and then the lateral error can be calculated as:
+
+        lateral_error = d_expected - d_actual.
+        
+    '''
+    pass
+
+def get_heading_error():
+    '''
+        The heading error will be the angular difference between the middle point
+        of the camera to the middle point of the track.
+
+        This can then be calculated as:
+
+        Cos(psi) = theta_normal / theta_actual
+
+        where theta_normal is the number of pixels in the bottom row that make up the lane on a perfectly straight road.
+              theta_actual is the actual number of pixels in the bottom row that make up the lane at the moment.
+    '''
+    pass
+
+def get_curvature():
+    '''
+        Draws a line from two top points and another line from two bottom points.
+        The angle between will be calculated as the curvature.
+
+        This can be done using tan.
+    '''
+    pass
+
+def get_pw_to_cm_conversion_constant(right_lane, left_lane):
+    return ( right_lane-left_lane ) / 45 
+
+ 
+def controller():
+    '''
+        The control algorithm driving the car.
+
+         κ′=−Kp(ye+Kdsinψe)+some_constant * κ
+
+
+    '''
+    pass
 
 
 
